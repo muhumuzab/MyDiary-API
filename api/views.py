a8719bd54ae74ba2d21042d782b5b854326ad079
Muhumuza Brian <muhumuzabr@gmail.com>
2018-07-19 06:16:36 -0400

    [task] setup home view

--- api/views.py
+++ api/views.py
@@ -5,3 +5,11 @@ from classes.item import DiaryItem
 from api import create_app
 
 app = create_app('DevelopmentEnv')
+
+@app.route('/')
+def index():
+    """
+    Index route
+    :return: 
+    """
+    return render_template('index.html')
