41d383ddf71f986774c32b23ac7d4de343bfb543
Bright Almighty <muhumuzabr@gmail.com>
2018-07-23 02:30:00 +0300

    add functionality for adding diary entry

--- application/views/views.py
+++ application/views/views.py
@@ -17,4 +17,33 @@ entry = api.model('Diary Entry', {
 
 
 
+class Entries(Resource):
+    
 
+    @api.doc(responses={'message': 'diary entry added successfully.',
+                        201: 'Created', 400: 'BAD FORMAT'})
+    @api.expect(entry)
+    def post(self):
+        """Create an entry."""
+        data = request.get_json()
+        # Check whether there is data
+
+        if any(data):
+            # save entry to data structure
+            
+            
+            # set id for the entry offer
+            diary_entry = DiaryEntry(data)
+            entry_id = len(entries) + 1
+            entries[(entry_id)] = diary_entry.getDict()
+            response = {'message': 'diary entry added successfully.',
+                            'entry id': entry_id}
+            return (response), 201
+        else:
+            return {'message': 'make sure you provide all required fields.'}, 400
+
+
+
+    
+
+api.add_resource(Entries, '/entries')
