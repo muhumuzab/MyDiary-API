60e5d388700dcee4ef8637dba23c71eff75e67f4
Bright Almighty <muhumuzabr@gmail.com>
2018-07-25 00:15:35 +0300

    [fix] adjust environment variables

--- instance/config.py
+++ instance/config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = False
+    DEBUG = True
     TESTING = False
 
 
--- run.py
+++ run.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     app = create_app('default')
     app.wsgi_app = ProxyFix(app.wsgi_app)
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
